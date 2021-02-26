9c77d49a53bfd020861349b3c75738acb5e8e1d1
jinguobiao <jinguobiao@wpmkbj.com>
2021-02-26 10:34:31 +0800

    commit code

--- examples/main.js
+++ examples/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import 'element-ui/lib/theme-chalk/index.css';
+import { Select, Option, Tree } from 'element-ui';
+import treeSelect from '@/index';
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(Tree);
+Vue.use(treeSelect);
 Vue.config.productionTip = false
 
 new Vue({
